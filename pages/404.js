72182f922c3a35aa059da7ac947f3e3865b19fdc
MasturFYC <mastur.st12@gmail.com>
2021-09-15 14:41:24 +0700

    New Book Date - 2

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,4 @@
 import Link from '@/components/Link'
-
 export default function FourZeroFour() {
   return (
     <div className="flex flex-col items-start justify-start md:justify-center md:items-center md:flex-row md:space-x-6 md:mt-24">
